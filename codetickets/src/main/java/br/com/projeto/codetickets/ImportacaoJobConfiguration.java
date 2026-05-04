1ede047cff5d8cd114964a36a61337960ca189dd
ArturLLopes <arturllopes@gmail.com>
2026-05-04 16:49:27 -0300

    Feat: Implementação do passo inicial do job

--- codetickets/src/main/java/br/com/projeto/codetickets/ImportacaoJobConfiguration.java
+++ codetickets/src/main/java/br/com/projeto/codetickets/ImportacaoJobConfiguration.java
@@ -5,14 +5,33 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class ImportacaoJobConfiguration {
+
+    @Autowired
+    private PlatformTransactionManager TransactionManager;
+
     public Job job(Step passoInicial, JobRepository jobRepository) {
         return new JobBuilder("geracao-tickets", jobRepository)
                 .start(passoInicial)
                 .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step passoInicial(ItemReader<Importacao> reader, ItemWriter<Importacao>writer, JobRepository jobRepository) {
+        return new StepBuilder("passo-inicial", jobRepository)
+                .<Importacao, Importacao>chunk(200, TransactionManager)
+                .reader(reader)
+                .writer(writer)
                 .build();
     }
 }
